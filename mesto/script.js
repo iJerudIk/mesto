e992f41a056d6db9f3c64bf009846a7a5796ba58
Aleksey Shinkevich <iJerudIk@gmail.com>
2021-11-19 19:23:42 +0300

    added: adtising

--- mesto/script.js
+++ mesto/script.js
@@ -1,5 +1,30 @@
-closeButton.addEventListener('submit', function (){
-  closePopup();
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-})
+let closeButton = document.querySelector('.popup__close-button');
+let editButton = document.querySelector('.profile__edit-button');
+let formElement = document.querySelector('.popup__form');
+
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+
+let inputName = document.querySelector('.form__name');
+let inputJob = document.querySelector('.form__job');
+
+let popup = document.querySelector('.popup');
+
+
+function togglePopup(){
+  popup.classList.toggle('popup_opened');
+}
+function formSubmitHandler(evt){
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  togglePopup();
+}
+
+editButton.addEventListener('click', togglePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener('click', function(){
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+  togglePopup();
+});
