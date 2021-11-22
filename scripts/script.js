9251b8d6923e9c5f9d8d25981ea512fd627ddb02
Aleksey Shinkevich <iJerudIk@gmail.com>
2021-11-22 16:13:43 +0300

    fixed: review 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,30 @@
 let closeButton = document.querySelector('.popup__close-button');
 let editButton = document.querySelector('.profile__edit-button');
 let formElement = document.querySelector('.popup__form');
-
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-
-let inputName = document.querySelector('.popup__field-name');
-let inputJob = document.querySelector('.popup__field-job');
-
+let inputName = document.querySelector('.popup__input_content_name');
+let inputJob = document.querySelector('.popup__input_content_job');
 let popup = document.querySelector('.popup');
 
-inputName.value = profileName.textContent;
-inputJob.value = profileJob.textContent;
-
-function togglePopup(){
-  popup.classList.toggle('popup_opened');
-}
-function formSubmitHandler(evt){
-  evt.preventDefault();
-  console.log(profileName.textContent);
-  profileName.textContent = inputName.value;
-  profileJob.textContent = inputJob.value;
-  togglePopup();
-}
-
-editButton.addEventListener('click', togglePopup);
-formElement.addEventListener('submit', formSubmitHandler);
-closeButton.addEventListener('click', function(){
+function openPopup(){
+  popup.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
-  togglePopup();
-});
+}
+function closePopup(){
+  popup.classList.remove('popup_opened');
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+}
+
+function formSubmitHandler(evt){
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+formElement.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener('click', closePopup);
